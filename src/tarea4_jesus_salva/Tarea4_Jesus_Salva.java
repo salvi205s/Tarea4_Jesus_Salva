62d6cc9d50c896f8e5d3f0a25ee42b5d5a8d405e
jesusledesma15 <jesusproint998@gmail.com>
2020-01-10 20:01:26 +0100

    Añade getters y setters

--- src/tarea4_jesus_salva/Tarea4_Jesus_Salva.java
+++ src/tarea4_jesus_salva/Tarea4_Jesus_Salva.java
@@ -23,6 +23,23 @@ public class Tarea4_Jesus_Salva {
         this.nombre = "Salvisus";
         this.edad = 58; //Entre los dos
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
     
+       
     
 }
