aef7f11e06c00857c9dac2148e18c5769ea46552
jesusledesma15 <jesusproint998@gmail.com>
2020-01-10 20:05:05 +0100

    Añade clase main

---
+++ src/tarea4_jesus_salva/Prueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4_jesus_salva;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+        Tarea4_Jesus_Salva obj1 = new Tarea4_Jesus_Salva();
+        Tarea4_Jesus_Salva obj2 = new Tarea4_Jesus_Salva();
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+    
+}
