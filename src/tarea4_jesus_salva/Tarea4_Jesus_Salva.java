1310f8f4253400c381545315e333b7d7dad1c6a2
salvi205 <salvi205s@gmail.com>
2020-01-10 19:33:23 +0100

    Añade atributos en clase modelo

---
+++ src/tarea4_jesus_salva/Tarea4_Jesus_Salva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4_jesus_salva;
+
+/**
+ *
+ * @author Salva
+ */
+public class Tarea4_Jesus_Salva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
